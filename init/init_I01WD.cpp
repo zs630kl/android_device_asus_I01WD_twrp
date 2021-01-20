4105bb2559ea3ce500b7630d7d58f425a66c8354
micky387 <mickaelsaibi@free.fr>
2021-01-20 08:41:12 +0100

    I01WD: Convert property_set to override
    Change-Id: I3868fc1307da3a53ed14b40bc30371ef83962532

--- init/init_I01WD.cpp
+++ init/init_I01WD.cpp
@@ -33,16 +33,33 @@
 #include <android-base/logging.h>
 #include <android-base/properties.h>
 
-#include "property_service.h"
-#include "log.h"
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 namespace android {
 namespace init {
 
+void property_override(const std::string& name, const std::string& value)
+{
+    size_t valuelen = value.size();
+
+    prop_info* pi = (prop_info*) __system_property_find(name.c_str());
+    if (pi != nullptr) {
+        __system_property_update(pi, value.c_str(), valuelen);
+    }
+    else {
+        int rc = __system_property_add(name.c_str(), name.size(), value.c_str(), valuelen);
+        if (rc < 0) {
+            LOG(ERROR) << "property_set(\"" << name << "\", \"" << value << "\") failed: "
+                       << "__system_property_add failed";
+        }
+    }
+}
+
 void vendor_load_properties() {
-    property_set("ro.bootimage.build.date.utc", "1546335651");
-    property_set("ro.build.date.utc", "1546335651");
-	}
+    property_override("ro.bootimage.build.date.utc", "1546335651");
+    property_override("ro.build.date.utc", "1546335651");
+    }
 
 }  // namespace init
 }  // namespace android
